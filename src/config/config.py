d2d87eee2d7a9a7d079ca822ccb3a818954490ca
Xi Yan <yanxi970830@gmail.com>
2019-02-16 00:12:59 -0500

    classnames

--- src/config/config.py
+++ src/config/config.py
@@ -7,136 +7,142 @@ import os.path as osp
 import numpy as np
 from easydict import EasyDict as edict
 
+
 def base_model_config(dataset='PASCAL_VOC'):
-  assert dataset.upper()=='PASCAL_VOC' or dataset.upper()=='KITTI', \
-      'Currently only support PASCAL_VOC or KITTI dataset'
+    assert dataset.upper() == 'PASCAL_VOC' or dataset.upper() == 'KITTI', \
+        'Currently only support PASCAL_VOC or KITTI dataset'
 
-  cfg = edict()
+    cfg = edict()
 
-  # Dataset used to train/val/test model. Now support PASCAL_VOC or KITTI
-  cfg.DATASET = dataset.upper()
+    # Dataset used to train/val/test model. Now support PASCAL_VOC or KITTI
+    cfg.DATASET = dataset.upper()
 
-  if cfg.DATASET == 'PASCAL_VOC':
-    # object categories to classify
-    cfg.CLASS_NAMES = ('aeroplane', 'bicycle', 'bird', 'boat', 'bottle', 'bus',
-                       'car', 'cat', 'chair', 'cow', 'diningtable', 'dog',
-                       'horse', 'motorbike', 'person', 'pottedplant', 'sheep',
-                       'sofa', 'train', 'tvmonitor')
-  elif cfg.DATASET == 'KITTI':
-    cfg.CLASS_NAMES = ('car', 'pedestrian', 'cyclist')
+    if cfg.DATASET == 'PASCAL_VOC':
+        # object categories to classify
+        cfg.CLASS_NAMES = ('aeroplane', 'bicycle', 'bird', 'boat', 'bottle', 'bus',
+                           'car', 'cat', 'chair', 'cow', 'diningtable', 'dog',
+                           'horse', 'motorbike', 'person', 'pottedplant', 'sheep',
+                           'sofa', 'train', 'tvmonitor')
+    elif cfg.DATASET == 'KITTI':
+        # cfg.CLASS_NAMES = ('car', 'pedestrian', 'cyclist')
+        # cfg.CLASS_NAMES = ('red', 'green', 'off')
+        cfg.CLASS_NAMES = (
+        'donotenter', 'handicappedparking', 'leftarrow', 'leftturnonly', 'rightarrow', 'rightturnonly', 'speedlimit10',
+        'speedlimit15', 'speedlimit20', 'speedlimit5', 'red', 'green', 'off')
 
-  # number of categories to classify
-  cfg.CLASSES = len(cfg.CLASS_NAMES)    
 
-  # ROI pooling output width
-  cfg.GRID_POOL_WIDTH = 7
+    # number of categories to classify
+    cfg.CLASSES = len(cfg.CLASS_NAMES)
 
-  # ROI pooling output height
-  cfg.GRID_POOL_HEIGHT = 7
+    # ROI pooling output width
+    cfg.GRID_POOL_WIDTH = 7
 
-  # parameter used in leaky ReLU
-  cfg.LEAKY_COEF = 0.1
+    # ROI pooling output height
+    cfg.GRID_POOL_HEIGHT = 7
 
-  # Probability to keep a node in dropout
-  cfg.KEEP_PROB = 0.5
+    # parameter used in leaky ReLU
+    cfg.LEAKY_COEF = 0.1
 
-  # image width
-  cfg.IMAGE_WIDTH = 224
+    # Probability to keep a node in dropout
+    cfg.KEEP_PROB = 0.5
 
-  # image height
-  cfg.IMAGE_HEIGHT = 224
+    # image width
+    cfg.IMAGE_WIDTH = 224
 
-  # anchor box, array of [cx, cy, w, h]. To be defined later
-  cfg.ANCHOR_BOX = []
+    # image height
+    cfg.IMAGE_HEIGHT = 224
 
-  # number of anchor boxes
-  cfg.ANCHORS = len(cfg.ANCHOR_BOX)
+    # anchor box, array of [cx, cy, w, h]. To be defined later
+    cfg.ANCHOR_BOX = []
 
-  # number of anchor boxes per grid
-  cfg.ANCHOR_PER_GRID = -1
+    # number of anchor boxes
+    cfg.ANCHORS = len(cfg.ANCHOR_BOX)
 
-  # batch size
-  cfg.BATCH_SIZE = 20
+    # number of anchor boxes per grid
+    cfg.ANCHOR_PER_GRID = -1
 
-  # Only keep boxes with probability higher than this threshold
-  cfg.PROB_THRESH = 0.005
+    # batch size
+    cfg.BATCH_SIZE = 20
 
-  # Only plot boxes with probability higher than this threshold
-  cfg.PLOT_PROB_THRESH = 0.5
+    # Only keep boxes with probability higher than this threshold
+    cfg.PROB_THRESH = 0.005
 
-  # Bounding boxes with IOU larger than this are going to be removed
-  cfg.NMS_THRESH = 0.2
+    # Only plot boxes with probability higher than this threshold
+    cfg.PLOT_PROB_THRESH = 0.5
 
-  # Pixel mean values (BGR order) as a (1, 1, 3) array. Below is the BGR mean
-  # of VGG16
-  cfg.BGR_MEANS = np.array([[[103.939, 116.779, 123.68]]])
+    # Bounding boxes with IOU larger than this are going to be removed
+    cfg.NMS_THRESH = 0.2
 
-  # loss coefficient for confidence regression
-  cfg.LOSS_COEF_CONF = 1.0
+    # Pixel mean values (BGR order) as a (1, 1, 3) array. Below is the BGR mean
+    # of VGG16
+    cfg.BGR_MEANS = np.array([[[103.939, 116.779, 123.68]]])
 
-  # loss coefficient for classification regression
-  cfg.LOSS_COEF_CLASS = 1.0
+    # loss coefficient for confidence regression
+    cfg.LOSS_COEF_CONF = 1.0
 
-  # loss coefficient for bounding box regression
-  cfg.LOSS_COEF_BBOX = 10.0
-                           
-  # reduce step size after this many steps
-  cfg.DECAY_STEPS = 10000
+    # loss coefficient for classification regression
+    cfg.LOSS_COEF_CLASS = 1.0
 
-  # multiply the learning rate by this factor
-  cfg.LR_DECAY_FACTOR = 0.1
+    # loss coefficient for bounding box regression
+    cfg.LOSS_COEF_BBOX = 10.0
 
-  # learning rate
-  cfg.LEARNING_RATE = 0.005
+    # reduce step size after this many steps
+    cfg.DECAY_STEPS = 10000
 
-  # momentum
-  cfg.MOMENTUM = 0.9
+    # multiply the learning rate by this factor
+    cfg.LR_DECAY_FACTOR = 0.1
 
-  # weight decay
-  cfg.WEIGHT_DECAY = 0.0005
+    # learning rate
+    cfg.LEARNING_RATE = 0.005
 
-  # wether to load pre-trained model
-  cfg.LOAD_PRETRAINED_MODEL = True
+    # momentum
+    cfg.MOMENTUM = 0.9
 
-  # path to load the pre-trained model
-  cfg.PRETRAINED_MODEL_PATH = ''
+    # weight decay
+    cfg.WEIGHT_DECAY = 0.0005
 
-  # print log to console in debug mode
-  cfg.DEBUG_MODE = False
+    # wether to load pre-trained model
+    cfg.LOAD_PRETRAINED_MODEL = True
 
-  # a small value used to prevent numerical instability
-  cfg.EPSILON = 1e-16
+    # path to load the pre-trained model
+    cfg.PRETRAINED_MODEL_PATH = ''
 
-  # threshold for safe exponential operation
-  cfg.EXP_THRESH=1.0
+    # print log to console in debug mode
+    cfg.DEBUG_MODE = False
 
-  # gradients with norm larger than this is going to be clipped.
-  cfg.MAX_GRAD_NORM = 10.0
+    # a small value used to prevent numerical instability
+    cfg.EPSILON = 1e-16
 
-  # Whether to do data augmentation
-  cfg.DATA_AUGMENTATION = False
+    # threshold for safe exponential operation
+    cfg.EXP_THRESH = 1.0
 
-  # The range to randomly shift the image widht
-  cfg.DRIFT_X = 0
+    # gradients with norm larger than this is going to be clipped.
+    cfg.MAX_GRAD_NORM = 10.0
 
-  # The range to randomly shift the image height
-  cfg.DRIFT_Y = 0
+    # Whether to do data augmentation
+    cfg.DATA_AUGMENTATION = False
 
-  # Whether to exclude images harder than hard-category. Only useful for KITTI
-  # dataset.
-  cfg.EXCLUDE_HARD_EXAMPLES = True
+    # The range to randomly shift the image widht
+    cfg.DRIFT_X = 0
 
-  # small value used in batch normalization to prevent dividing by 0. The
-  # default value here is the same with caffe's default value.
-  cfg.BATCH_NORM_EPSILON = 1e-5
+    # The range to randomly shift the image height
+    cfg.DRIFT_Y = 0
 
-  # number of threads to fetch data
-  cfg.NUM_THREAD = 4
+    # Whether to exclude images harder than hard-category. Only useful for KITTI
+    # dataset.
+    cfg.EXCLUDE_HARD_EXAMPLES = True
 
-  # capacity for FIFOQueue
-  cfg.QUEUE_CAPACITY = 100
+    # small value used in batch normalization to prevent dividing by 0. The
+    # default value here is the same with caffe's default value.
+    cfg.BATCH_NORM_EPSILON = 1e-5
 
-  # indicate if the model is in training mode
-  cfg.IS_TRAINING = False
+    # number of threads to fetch data
+    cfg.NUM_THREAD = 4
 
-  return cfg
+    # capacity for FIFOQueue
+    cfg.QUEUE_CAPACITY = 100
+
+    # indicate if the model is in training mode
+    cfg.IS_TRAINING = False
+
+    return cfg
