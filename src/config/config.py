69494095ca97a2b5f6453e10832791171fd2d1f9
Xi Yan <yanxi970830@gmail.com>
2019-03-26 19:24:08 -0400

    scale signs class labels

--- src/config/config.py
+++ src/config/config.py
@@ -26,10 +26,10 @@ def base_model_config(dataset='PASCAL_VOC'):
     elif cfg.DATASET == 'KITTI':
         # cfg.CLASS_NAMES = ('car', 'pedestrian', 'cyclist')
         # cfg.CLASS_NAMES = ('red', 'green', 'off')
-        cfg.CLASS_NAMES = (
-        'donotenter', 'handicappedparking', 'leftarrow', 'leftturnonly', 'rightarrow', 'rightturnonly', 'speedlimit10',
-        'speedlimit15', 'speedlimit20', 'speedlimit5', 'red', 'green', 'off', 'sign', 'light')
-
+        # cfg.CLASS_NAMES = (
+        # 'donotenter', 'handicappedparking', 'leftarrow', 'leftturnonly', 'rightarrow', 'rightturnonly', 'speedlimit10',
+        # 'speedlimit15', 'speedlimit20', 'speedlimit5', 'red', 'green', 'off', 'sign', 'light')
+        cfg.CLASS_NAMES = ('Handicap Parking', 'Stop', 'Do Not Enter', 'Left Turn Only (arrow)', 'Left Turn Only (words)', 'Speed Limit 5', 'Speed Limit 10', 'Speed Limit 15', 'Speed Limit 20', 'Railroad Sign', 'Railroad Light Pair On', 'Railroad Light Pair Off')
 
     # number of categories to classify
     cfg.CLASSES = len(cfg.CLASS_NAMES)
